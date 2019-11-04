618d61ddbc810a5aafb69ea4ce8c7111f3892d03
Luca <lucabaronih@gmail.com>
2019-11-04 21:44:32 +0100

    Use a bounded queue for next and previous functions

--- slideshow/script.js
+++ slideshow/script.js
@@ -8,15 +8,18 @@ const modal = document.getElementById('modal');
 const modalImg = document.getElementById('modal-img');
 const hideModalButton = document.getElementById('modal-hide-button');
 
-let backgrounds = ['green', 'blue', 'orange', 'red', 'purple'];
+const backgrounds = ['green', 'blue', 'orange', 'red', 'purple'];
+const bgLength = backgrounds.length;
 let animationInProgress = false;
+// head is the index of the front card in the backgrounds array
+let head = 0;
 
 function next() {
   if (animationInProgress) {
     return;
   }
   animationInProgress = true;
-  // the slideshow animation is active only if the screen is sufficiently large
+  // the slideshow animation gets triggered only if the screen is sufficiently large
   if (window.outerWidth > 800) {
     front.style.animationName = 'forward_transition_front';
     tile1.style.animationName = 'forward_transition_tile1';
@@ -25,14 +28,14 @@ function next() {
     back.style.animationName = 'forward_transition_back';
   }
 
-  backgrounds = [backgrounds.pop()].concat(backgrounds);
+  head = (bgLength + head - 1) % bgLength;
 
   setTimeout(function() {
-    front.style.backgroundColor = backgrounds[0];
-    tile1.style.backgroundColor = backgrounds[1];
-    tile2.style.backgroundColor = backgrounds[2];
-    tile3.style.backgroundColor = backgrounds[3];
-    back.style.backgroundColor = backgrounds[4];
+    front.style.backgroundColor = backgrounds[head];
+    tile1.style.backgroundColor = backgrounds[(head + 1) % bgLength];
+    tile2.style.backgroundColor = backgrounds[(head + 2) % bgLength];
+    tile3.style.backgroundColor = backgrounds[(head + 3) % bgLength];
+    back.style.backgroundColor = backgrounds[(head + 4) % bgLength];
     front.style.animationName = 'none';
     tile1.style.animationName = 'none';
     tile2.style.animationName = 'none';
@@ -54,13 +57,15 @@ function previous() {
     tile1.style.animationName = 'backward_transition_tile1';
     front.style.animationName = 'backward_transition_front';
   }
-  backgrounds = backgrounds.concat([backgrounds.shift()]);
+
+  head = (head + 1) % bgLength;
+
   setTimeout(function() {
-    front.style.backgroundColor = backgrounds[0];
-    tile1.style.backgroundColor = backgrounds[1];
-    tile2.style.backgroundColor = backgrounds[2];
-    tile3.style.backgroundColor = backgrounds[3];
-    back.style.backgroundColor = backgrounds[4];
+    front.style.backgroundColor = backgrounds[head];
+    tile1.style.backgroundColor = backgrounds[(head + 1) % bgLength];
+    tile2.style.backgroundColor = backgrounds[(head + 2) % bgLength];
+    tile3.style.backgroundColor = backgrounds[(head + 3) % bgLength];
+    back.style.backgroundColor = backgrounds[(head + 4) % bgLength];
     back.style.animationName = 'none';
     tile3.style.animationName = 'none';
     tile2.style.animationName = 'none';
@@ -73,7 +78,7 @@ function previous() {
 function showModal() {
   document.body.style.overflowY = 'hidden';
   modal.style.display = 'block';
-  modalImg.style.backgroundColor = backgrounds[1];
+  modalImg.style.backgroundColor = backgrounds[(head + 1) % bgLength];
 }
 
 function hideModal() {
