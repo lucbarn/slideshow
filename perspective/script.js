38a97645880198c3dc8db8099cf32c3a3c2894ba
Luca <lucabaronih@gmail.com>
2019-11-09 21:04:39 +0100

    New scale coefficient based on the height of the browser's window

--- perspective/script.js
+++ perspective/script.js
@@ -8,9 +8,12 @@ const elements = [front, card1, card2, card3, back];
 const x0 = 720;
 const y0 = 240;
 const widthHeightRatio = 4 / 3;
-// px and py are the coordinates of the point of view
+// px and py are the coordinates of the point of view on the cartesian plane
 let px = x0;
 let py = y0;
+let windowHeight = window.innerHeight;
+// the theoretical values of the height of each card and the space between the
+// the cards; the first card is placed on the y-axis, the others to its left
 const cardsHeight = 480;
 const spaceBetweenCards = 100;
 
@@ -21,13 +24,22 @@ function updateCards() {
   // y2 is the intersection between the line that connects the top of the card with
   // the point of view and the y axis
   let y2;
+  // given a viewing angle of 90 degrees, the height of the field of view is twice
+  // the distance of the point of view from the y axis; assuming that the field of
+  // view covers the height of the browser window, then the ratio between the
+  // height of the field of view and the height of each card is equal to the height
+  // of the browser window and the height of the first card displayed on the screen;
+  // 2*px / cardsHeight == windowHeight / displayedCardsHeight or
+  // displayedCardsHeight / cardsHeight == windowHeight / 2*px;
+  // the scale coefficient is the ratio between the height of the displayed first
+  // card and the theoretical height of each card
   let scaleCoefficient;
   let height;
   let translation;
   for (let i = 0; i < 5; i++) {
     y1 = (py * spaceBetweenCards * i) / (px + spaceBetweenCards * i);
     y2 = (cardsHeight * px + py * spaceBetweenCards * i) / (px + spaceBetweenCards * i);
-    scaleCoefficient = cardsHeight / px;
+    scaleCoefficient = windowHeight / (2 * px);
     height = (y2-y1) * scaleCoefficient;
     translation = y1 * scaleCoefficient;
     elements[i].style.height = height + 'px';
