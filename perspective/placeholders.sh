7ed74bf947fccc799492b04de7164956dc608a9b
Luca <lucabaronih@gmail.com>
2019-11-30 18:16:49 +0100

    Update bash script and add some placeholders

--- perspective/placeholders.sh
+++ perspective/placeholders.sh
@@ -1,8 +1,26 @@
 IFS="|"
+
+cp style_template.css style.css
+cp script_template.js script.js
+
 while read placeholder value
 do
   if [[ ( "$placeholder" =~ [^[:space:]] ) && ( "$value" =~ [^[:space:]] ) ]]
   then
-    echo "$placeholder $value"
+    sed -i "s/$placeholder/$value/g" style.css
+    sed -i "s/$placeholder/$value/g" script.js
+
   fi
 done < $1
+
+count1=$(eval grep "_placeholder" style.css | wc -l)
+if [[ $count1 -gt 0 ]]
+then
+  echo "Warning: some placeholders in style_template.css could not be replaced."
+fi
+
+count2=$(eval grep "_placeholder" script.js | wc -l)
+if [[ $count2 -gt 0 ]]
+then
+  echo "Warning: some placeholders in script_template.js could not be replaced."
+fi
---
+++ perspective/script_template.js
@@ -0,0 +1,93 @@
+const front = document.getElementById('front');
+const card1 = document.getElementById('card1');
+const card2 = document.getElementById('card2');
+const card3 = document.getElementById('card3');
+const back = document.getElementById('back');
+const cardsContainer = document.getElementById('cards-container');
+const elements = [front, card1, card2, card3, back];
+const x0 = 720;
+const y0 = 240;
+const widthHeightRatio = 4 / 3;
+// the theoretical values of the height of each card and the space between the
+// the cards; the first card is placed on the y-axis, the others to its left
+const cardsHeight = 480;
+const spaceBetweenCards = 100;
+// px and py are the coordinates of the point of view on the cartesian plane
+let px = x0;
+let py = y0;
+let windowHeight = window.innerHeight;
+
+function updateCards() {
+  // y1 is the intersection between the line that connects the base of the card with
+  // the point of view and the y axis
+  let y1;
+  // y2 is the intersection between the line that connects the top of the card with
+  // the point of view and the y axis
+  let y2;
+  // given a viewing angle of 90 degrees, the height of the field of view is twice
+  // the distance of the point of view from the y axis; assuming that the field of
+  // view covers the height of the browser window, then the ratio between the
+  // height of the field of view and the height of each card is equal to the height
+  // of the browser window and the height of the first card displayed on the screen;
+  // 2*px / cardsHeight == windowHeight / displayedCardsHeight or
+  // displayedCardsHeight / cardsHeight == windowHeight / 2*px;
+  // the scale coefficient is the ratio between the height of the displayed first
+  // card and the theoretical height of each card
+  let scaleCoefficient;
+  let height;
+  let translation;
+  for (let i = 0; i < 5; i++) {
+    y1 = (py * spaceBetweenCards * i) / (px + spaceBetweenCards * i);
+    y2 = (cardsHeight * px + py * spaceBetweenCards * i) / (px + spaceBetweenCards * i);
+    scaleCoefficient = windowHeight / (2 * px);
+    height = (y2-y1) * scaleCoefficient;
+    translation = y1 * scaleCoefficient;
+    elements[i].style.height = height + 'px';
+    elements[i].style.width = height * widthHeightRatio + 'px';
+    elements[i].style.transform = 'translateY(' + -1 * translation + 'px) translateX(-50%)';
+  }
+  cardsContainer.style.width = front.style.width;
+  cardsContainer.style.height = front.style.height;
+}
+
+updateCards();
+
+const pov = document.getElementById('pov');
+const povArea = document.getElementById('pov-area');
+let cornerX = povArea.getBoundingClientRect().x;
+let cornerY = povArea.getBoundingClientRect().y;
+const xLimit = pov_container_side_int_placeholder;
+const yLimit = pov_container_side_int_placeholder;
+
+function movePov(event) {
+  event.preventDefault();
+  const x = event.clientX;
+  const y = event.clientY;
+  let xPos;
+  let yPos;
+  if (x > cornerX + xLimit / 2) {
+    xPos = Math.min(x - cornerX, xLimit);
+  } else {
+    xPos = Math.max(x - cornerX, 0);
+  }
+  if (y > cornerY + yLimit / 2) {
+    yPos = Math.min(y - cornerY, yLimit);
+  } else {
+    yPos = Math.max(y - cornerY, 0);
+  }
+  pov.style.left = `${xPos}px`;
+  pov.style.top = `${yPos}px`;
+  px = (xPos + 80) * 4;
+  py = (yLimit - yPos - 40) * 4;
+  updateCards();
+}
+
+pov.addEventListener('mousedown', function() {
+  povArea.style.backgroundColor = 'rgba(0,0,100,0.1)';
+  document.addEventListener('mousemove', movePov);
+});
+
+document.addEventListener('mouseup', function() {
+  povArea.style.backgroundColor = '';
+  document.removeEventListener('mousemove', movePov);
+});
